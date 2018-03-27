5b867d17a3dd626188b2f8b4742ad3784ca198e0
Michal Bruzdowski <bruzdowski@gmail.com>
2018-03-27 21:25:47 +0200

    Add Vector Graphic

--- app/src/main/java/pl/michal/choplifterv2/gui/GameObject.java
+++ app/src/main/java/pl/michal/choplifterv2/gui/GameObject.java
@@ -2,12 +2,16 @@ package pl.michal.choplifterv2.gui;
 
 import android.graphics.Canvas;
 
+import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+
 /**
  * Created by micha on 14.03.2018.
  */
 
 public interface GameObject {
-    public void draw(Canvas canvas);
+    public void draw(Canvas canvas) throws SVGParseException, IOException;
         public void update();
 
 
